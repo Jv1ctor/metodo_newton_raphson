9a7cf1d3f4c8ee8e51fb39b41267935f8fe26d87
João Victor <j.rodriguesbarretos@gmail.com>
2024-10-29 23:35:09 -0300

    concluido

--- newton_raphson.py
+++ newton_raphson.py
@@ -1,5 +1,7 @@
-matricula = ["2422976", "2422970", "2422983", "2422977", "2422993"]
-lastNumbersForMatricula = list(map(lambda item: int(item[-1]), matricula)) # separa o numero final das matriculas
+import os
+# A lista da matricula de voces
+matricula = [2422976, 2422970, 2422983, 2422977, 2422993]
+lastNumbersForMatricula = list(map(lambda item: item%10, matricula)) # separa o numero final das matriculas
 
 #Calcula o N a partir das matriculas
 def calcN(list):
@@ -26,13 +28,55 @@ def newton_raphson(n_velo,fun, val0, tol = 1e-8, maxIter = 100):
       
       val0 = val  
 
+def handle_newton_raphson(velocity_n, fun, initial_value, max_value):
+  list_num_right = set()
+  list_num_left = set()
+  for i in range(initial_value, max_value): 
+    root = newton_raphson(velocity_n, fun, i)
+    value = fun(root)
+    if(value > N):
+      list_num_right.add(root)
+    elif(value < N):
+      list_num_left.add(root)
+    
+  return [list_num_right, list_num_left]
 
-# funcao da velocidade
+
+
+# funcao da velocidade - tua funcao Velocidade
 V = lambda x: -0.0000005181382 * x ** 8 + 0.0000615620817 * x ** 7 - 0.0030378878339 * x ** 6 + 0.0806132093984 * x ** 5 - 1.2432080780626 * x ** 4 + 11.213134252073 * x ** 3 - 56.1414827095908 * x ** 2 + 134.6598926750734 * x - 98.1605150163588
 
 # velocidade N 
 N = calcN(lastNumbersForMatricula)
 
+list_right, list_left = handle_newton_raphson(N, V, 0, 30)
+  
+list_left_ordered = sorted(list_left)
+list_right_ordered = sorted(list_right)
 
-# for i in range(0, 29): 
-print(newton_raphson(N, V, 1))
+loop = True
+while(loop):
+  print("\nEscolha uma das opcoes: ")
+  print("-- 0 para raizes aproximados pela Esquerda")
+  print("-- 1 para raizes aproximados pela Direita")
+  print("-- 2 Sair\n")
+  option = int(input())
+    
+
+  if(option == 0):
+    os.system("clear")
+    for i in list_left_ordered:
+      print(f"{i} -> {V(i)}")
+  
+  if(option == 1):
+    os.system("clear") 
+    for i in list_right_ordered:
+      print(f"{i} -> {V(i)}")
+
+  if(option == 2):
+    os.system("clear") 
+    loop = False
+
+
+
+
